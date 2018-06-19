18dc1b3589b40ed80633ec8af079e6d2efd11d64
Ben Ebel <bge1234@gmail.com>
2018-06-18 21:28:39 -0700

    Add testing instruction

--- lookForSubset.js
+++ lookForSubset.js
@@ -1,8 +1,10 @@
 /* Problem: Given a set of integers, is there a non-empty subset whose sum is zero? For example, given the set { -7, -3, -2, 8, 5}, the answer is yes because the subset { -3, -2, 5} sums to zero. It is OK to assume the set has no more than 16 elements.
 
-To run, pass the set as an array to subsetExists() (e.g. `subsetExists([ -7, -3, -2, 8, 5])`)
+To run, pass the set as an array to subsetExists() (e.g. `subsetExists([ -7, -3, -2, 8, 5])`).
 
-Output will be either "Yes" or "No" (the problem didn't ask to find the subset with the sum of zero, just to determine whether or not there is one). */
+Output will be either "Yes" or "No" (the problem didn't ask to find the subset with the sum of zero, just to determine whether or not there is one).
+
+To run the built in tests, run `node lookForSubset.js`. */
 
 function subsetExists(set) {
   var posNeg = positiveNegative(set)
